581f8cbd3e58d719b01b8537e5bbe43adcacab08
NULL_PTR <NULL_PTR@localhost>
2010-06-23 11:54:46 +0300

    Implemented some std_typeinfo_bind.cpp functions.

--- NULLC/translation/std_typeinfo_bind.cpp
+++ NULLC/translation/std_typeinfo_bind.cpp
@@ -17,53 +17,47 @@ struct argument_iterator
 };
 int isFunction(unsigned int type, void* unused)
 {
-	// $$$
-	return 0;
+	return __nullcGetTypeInfo(type)->category == NULLC_FUNCTION;
 }
 int isClass(unsigned int type, void* unused)
 {
-	// $$$
-	return 0;
+	return __nullcGetTypeInfo(type)->category == NULLC_CLASS;
 }
 int isSimple(unsigned int type, void* unused)
 {
-	// $$$
-	return 0;
+	// void double float long int short char
+	unsigned hash = __nullcGetTypeInfo(type)->hash;
+	return (hash == 2090838615u) || (hash == 4181547808u) || (hash == 259121563u) || (hash == 2090479413u) || (hash == 193495088u) || (hash == 274395349u) || (hash == 2090147939u);
 }
 int isArray(unsigned int type, void* unused)
 {
-	// $$$
-	return 0;
+	return __nullcGetTypeInfo(type)->category == NULLC_ARRAY;
 }
 int isPointer(unsigned int type, void* unused)
 {
-	// $$$
-	return 0;
+	return __nullcGetTypeInfo(type)->category == NULLC_POINTER;
 }
 int isFunction(NULLCRef type, void* unused)
 {
-	// $$$
-	return 0;
+	return __nullcGetTypeInfo(type.typeID)->category == NULLC_FUNCTION;
 }
 int isClass(NULLCRef type, void* unused)
 {
-	// $$$
-	return 0;
+	return __nullcGetTypeInfo(type.typeID)->category == NULLC_CLASS;
 }
 int isSimple(NULLCRef type, void* unused)
 {
-	// $$$
-	return 0;
+	// void double float long int short char
+	unsigned hash = __nullcGetTypeInfo(type.typeID)->hash;
+	return (hash == 2090838615u) || (hash == 4181547808u) || (hash == 259121563u) || (hash == 2090479413u) || (hash == 193495088u) || (hash == 274395349u) || (hash == 2090147939u);
 }
 int isArray(NULLCRef type, void* unused)
 {
-	// $$$
-	return 0;
+	return __nullcGetTypeInfo(type.typeID)->category == NULLC_ARRAY;
 }
 int isPointer(NULLCRef type, void* unused)
 {
-	// $$$
-	return 0;
+	return __nullcGetTypeInfo(type.typeID)->category == NULLC_POINTER;
 }
 int typeid__size__int_ref__(unsigned int * __context)
 {
@@ -72,12 +66,19 @@ int typeid__size__int_ref__(unsigned int * __context)
 NULLCArray<char> typeid__name__char___ref__(unsigned int * __context)
 {
 	NULLCArray<char> ret;
+	ret.ptr = (char*)__nullcGetTypeInfo(*__context)->name;
+	ret.size = strlen(__nullcGetTypeInfo(*__context)->name) + 1;
 	return ret;
 }
 int typeid__memberCount_int_ref__(unsigned int * __context)
 {
-	// $$$
-	return 0;
+	NULLCTypeInfo *exType = __nullcGetTypeInfo(*__context);
+	if(exType->category != NULLC_CLASS)
+	{
+		nullcThrowError("typeid::memberCount: type (%s) is not a class", exType->name);
+		return 0;
+	}
+	return exType->memberCount;
 }
 unsigned int typeid__memberType_typeid_ref_int_(int member, unsigned int * __context)
 {
@@ -91,13 +92,23 @@ NULLCArray<char> typeid__memberName_char___ref_int_(int member, unsigned int * _
 }
 unsigned int typeid__subType_typeid_ref__(unsigned int * __context)
 {
-	// $$$
-	return 0;
+	NULLCTypeInfo *type = __nullcGetTypeInfo(*__context);
+	if(type->category != NULLC_ARRAY && type->category != NULLC_POINTER)
+	{
+		nullcThrowError("typeid::subType received type (%s) that neither pointer nor array", type->name);
+		return ~0u;
+	}
+	return type->subTypeID;
 }
 int typeid__arraySize_int_ref__(unsigned int * __context)
 {
-	// $$$
-	return 0;
+	NULLCTypeInfo *type = __nullcGetTypeInfo(*__context);
+	if(type->category != NULLC_ARRAY)
+	{
+		nullcThrowError("typeid::arraySize received type (%s) that is not an array", type->name);
+		return -1;
+	}
+	return type->memberCount;
 }
 unsigned int typeid__returnType_typeid_ref__(unsigned int * __context)
 {
