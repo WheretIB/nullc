ac6ef85d93eadbbbc87d14c56278968ad0fddd68
WheretIB <WheretIB@gmail.com>
2011-03-21 11:00:36 +0200

    Wrapped StrAlgo into NULLC namespace.

--- NULLC/StrAlgo.h
+++ NULLC/StrAlgo.h
@@ -4,27 +4,30 @@
 
 #include "stdafx.h"
 
-unsigned int GetStringHash(const char *str);
-unsigned int GetStringHash(const char *str, const char *end);
-unsigned int StringHashContinue(unsigned int hash, const char *str);
-unsigned int StringHashContinue(unsigned int hash, const char *str, const char *end);
-
-char*	PrintInteger(char* str, int number);
-
-int		SafeSprintf(char* dst, size_t size, const char* src, ...);
-
-// A string that doesn't terminate with a \0 character
-class InplaceStr
+namespace NULLC
 {
-public:
-	InplaceStr(){ begin = NULL; end = NULL; }
-	// It is possible to construct it from \0-terminated string
-	explicit InplaceStr(const char *strBegin){ begin = strBegin; end = begin + strlen(begin); }
-	// And from non-terminating strings
-	InplaceStr(const char *strBegin, unsigned int length){ begin = strBegin; end = begin + length; }
-	InplaceStr(const char *strBegin, const char *strEnd){ begin = strBegin; end = strEnd; }
+	unsigned int GetStringHash(const char *str);
+	unsigned int GetStringHash(const char *str, const char *end);
+	unsigned int StringHashContinue(unsigned int hash, const char *str);
+	unsigned int StringHashContinue(unsigned int hash, const char *str, const char *end);
 
-	const char *begin, *end;
-};
+	char*	PrintInteger(char* str, int number);
+
+	int		SafeSprintf(char* dst, size_t size, const char* src, ...);
+
+	// A string that doesn't terminate with a \0 character
+	class InplaceStr
+	{
+	public:
+		InplaceStr(){ begin = NULL; end = NULL; }
+		// It is possible to construct it from \0-terminated string
+		explicit InplaceStr(const char *strBegin){ begin = strBegin; end = begin + strlen(begin); }
+		// And from non-terminating strings
+		InplaceStr(const char *strBegin, unsigned int length){ begin = strBegin; end = begin + length; }
+		InplaceStr(const char *strBegin, const char *strEnd){ begin = strBegin; end = strEnd; }
+
+		const char *begin, *end;
+	};
+}
 
 #endif
