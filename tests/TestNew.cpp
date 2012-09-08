ab5251eb4a57b4a26bf5b912750f01effde43cb4
WheretIB <WheretIB@gmail.com>
2012-09-08 13:03:10 +0300

    Any type can be allocated using the 'new' expression. Fixed compiler crash on derived type allocation constructor call. Fixed compiler error in reference type allocation custom construction expression.

--- tests/TestNew.cpp
+++ tests/TestNew.cpp
@@ -37,3 +37,21 @@ TEST_RESULT("Type constructor after new 3.", testConstructorAfterNew3, "0");
 
 const char	*testTypeChangeInArrayExpression = "int[] arr = new int[(int f(){ return 5; })()]; return arr.size;";
 TEST_RESULT("Type change in array size in new expression", testTypeChangeInArrayExpression, "5");
+
+const char	*testArrayTypeConstruction1 = "auto x = new (int[]); return typeof(x) == int[] ref;";
+TEST_RESULT("Array type allocation 1", testArrayTypeConstruction1, "1");
+
+const char	*testArrayTypeConstruction2 = "auto x = new (int[])[2]; return typeof(x) == int[][] && x.size == 2;";
+TEST_RESULT("Array type allocation 2", testArrayTypeConstruction2, "1");
+
+const char	*testArrayTypeConstruction3 = "auto x = new (int[][]); return typeof(x) == int[][] ref;";
+TEST_RESULT("Array type allocation 3", testArrayTypeConstruction3, "1");
+
+const char	*testArrayTypeConstruction4 = "auto x = new (int[][])(); return typeof(x) == int[][] ref;";
+TEST_RESULT("Array type allocation 4", testArrayTypeConstruction4, "1");
+
+const char	*testArrayTypeConstruction5 = "auto x = new (int[2][2]); return typeof(x) == int[2][2] ref;";
+TEST_RESULT("Array type allocation 5", testArrayTypeConstruction5, "1");
+
+const char	*testDerivedTypeCustomConstruction = "auto x = new (int ref)(){ *this = new int(5); }; return * *x;";
+TEST_RESULT("Custom construction of a derived type", testDerivedTypeCustomConstruction, "5");
