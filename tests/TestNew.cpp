9eb42d8c1613ed8e8f141d78900b74fb4e8b30f9
NULL_PTR <NULL_PTR@localhost>
2012-12-08 15:26:43 +0200

    Fixed errors with custom construction of derived types. Added default one argument constructor for derived types.

--- tests/TestNew.cpp
+++ tests/TestNew.cpp
@@ -53,5 +53,32 @@ TEST_RESULT("Array type allocation 4", testArrayTypeConstruction4, "1");
 const char	*testArrayTypeConstruction5 = "auto x = new (int[2][2]); return typeof(x) == int[2][2] ref;";
 TEST_RESULT("Array type allocation 5", testArrayTypeConstruction5, "1");
 
-const char	*testDerivedTypeCustomConstruction = "auto x = new (int ref)(){ *this = new int(5); }; return * *x;";
-TEST_RESULT("Custom construction of a derived type", testDerivedTypeCustomConstruction, "5");
+const char	*testDerivedTypeValueConstruction1 = "auto a = { 1, 2 }; auto ay = new (int[2])(a); return ay[0] + ay[1];";
+TEST_RESULT("Value construction of a derived type 1", testDerivedTypeValueConstruction1, "3");
+
+const char	*testDerivedTypeValueConstruction2 = "auto by = new (int[2])({ 3, 4 }); return by[0] + by[1];";
+TEST_RESULT("Value construction of a derived type 2", testDerivedTypeValueConstruction2, "7");
+
+const char	*testDerivedTypeValueConstruction3 = "auto cay = new (int[])({ 1, 2 }); return cay[0] + cay[1];";
+TEST_RESULT("Value construction of a derived type 3", testDerivedTypeValueConstruction3, "3");
+
+const char	*testDerivedTypeValueConstruction4 = "auto x = new int(5); auto dy = new int ref(x); return * *dy;";
+TEST_RESULT("Value construction of a derived type 4", testDerivedTypeValueConstruction4, "5");
+
+const char	*testDerivedTypeValueConstruction5 = "auto fy = new int ref()(auto(){ return 14; }); return (*fy)();";
+TEST_RESULT("Value construction of a derived type 5", testDerivedTypeValueConstruction5, "14");
+
+const char	*testDerivedTypeValueConstruction6 = "auto fy = new int ref(int)(auto(@T x){ return x + 4; }); return (*fy)(4);";
+TEST_RESULT("Value construction of a derived type 6", testDerivedTypeValueConstruction6, "8");
+
+const char	*testDerivedTypeCustomConstruction1 = "auto x = new (int ref)(){ *this = new int(5); }; return * *x;";
+TEST_RESULT("Custom construction of a derived type 1", testDerivedTypeCustomConstruction1, "5");
+
+const char	*testDerivedTypeCustomConstruction2 = "auto cy = new (int[2])(){ this[0] = 5; this[1] = 6; }; return cy[0] + cy[1];";
+TEST_RESULT("Custom construction of a derived type 2", testDerivedTypeCustomConstruction2, "11");
+
+const char	*testDerivedTypeCustomConstruction3 = "auto cby = new (int[])({ 1, 2, 3 }){ this[1] = 5; }; return cby[0] + cby[1] + cby[2];";
+TEST_RESULT("Custom construction of a derived type 3", testDerivedTypeCustomConstruction3, "9");
+
+const char	*testDerivedTypeCustomConstruction4 = "auto ccy = new (int[])({ 1, 2, 3 }){ for(i in *this) i *= 10; }; return ccy[0] + ccy[1] + ccy[2];";
+TEST_RESULT("Custom construction of a derived type 4", testDerivedTypeCustomConstruction4, "60");
