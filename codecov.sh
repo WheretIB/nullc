d17501ecd83076cbb8d4c0abb188eabe19b150d4
WheretIB <wheretib@gmail.com>
2019-02-21 21:58:13 +0200

    Re-enable coverage result upload

--- codecov.sh
+++ codecov.sh
@@ -1,4 +1,4 @@
-if [ "$CONFIG" == "coverage" ]; then
+if [[ ! ("$TRAVIS_COMPILER" == "clang") && ("$CONFIG" == "debug") && ("$EXTCALL" == "manual") ]]; then
   bash <(curl -s https://codecov.io/bash)
 else
   echo "No coverage data in this configuration"
