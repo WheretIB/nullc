6b2da768984cf03fc512411d69261c6d0ed6f5ab
WheretIB <wheretib@gmail.com>
2019-03-26 00:26:47 +0200

    Code coverage upload script fix

--- codecov.sh
+++ codecov.sh
@@ -1,4 +1,4 @@
-if [[ ! ("$TRAVIS_COMPILER" == "clang") && ("$CONFIG" == "debug") && ("$EXTCALL" == "manual") ]]; then
+if [[ ! ("$TRAVIS_COMPILER" == "clang") && ("$CONFIG" == "debug") ]]; then
   bash <(curl -s https://codecov.io/bash)
 else
   echo "No coverage data in this configuration"
