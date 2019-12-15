b3f4269868c704127948c9a4e2df2fd3cac65740
WheretIB <wheretib@gmail.com>
2019-12-15 23:59:48 +0200

    jit: fixed preprocessor condition

--- NULLC/CodeGen_X86.cpp
+++ NULLC/CodeGen_X86.cpp
@@ -486,25 +486,25 @@ x86XmmReg CodeGenGenericContext::RedirectRegister(x86XmmReg reg)
 
 x86Reg CodeGenGenericContext::GetReg()
 {
-#ifdef NULLC_OPTIMIZE_X86
-	static x86Reg regs[] = { rEAX, rEDX, rEDI, rECX };
-
-	// Simple rotation
-	x86Reg res = regs[currFreeReg];
-
-	if(res == rECX)
-		currFreeReg = 0;
-	else
-		currFreeReg += 1;
-
-	return res;
-#else
+#if defined(_M_X64)
 	static x86Reg regs[] = { rRAX, rRDX, rEDI, rESI, rR8, rR9, rR10, rR11, rR12 };
 
 	// Simple rotation
 	x86Reg res = regs[currFreeReg];
 
 	if(res == rR11)
+		currFreeReg = 0;
+	else
+		currFreeReg += 1;
+
+	return res;
+#else
+	static x86Reg regs[] = { rEAX, rEDX, rEDI, rECX };
+
+	// Simple rotation
+	x86Reg res = regs[currFreeReg];
+
+	if(res == rECX)
 		currFreeReg = 0;
 	else
 		currFreeReg += 1;
