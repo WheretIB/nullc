60e030d033b51276f9ae50f30438126c555af011
WheretIB <wheretib@gmail.com>
2019-12-15 23:49:19 +0200

    jit: fixed compilation warning

--- NULLC/Executor_X86.cpp
+++ NULLC/Executor_X86.cpp
@@ -1186,7 +1186,7 @@ bool ExecutorX86::TranslateToNative(bool enableLogFiles, OutputContext &output)
 	{
 		ExternFuncInfo &target = exLinker->exFunctions[i];
 
-		if(target.regVmAddress != ~0u && target.vmCodeSize != 0 && (codeJumpTargets[target.regVmAddress] >> 8) == 0)
+		if(target.regVmAddress != -1 && target.vmCodeSize != 0 && (codeJumpTargets[target.regVmAddress] >> 8) == 0)
 			codeJumpTargets[target.regVmAddress] |= 2 + (i << 8);
 	}
 
