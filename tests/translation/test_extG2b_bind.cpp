a7c760ef713f38303cb67fc797505a071bfad3d3
WheretIB <wheretib@gmail.com>
2020-06-14 11:29:53 +0300

    translation to C: fixed test binding file

--- tests/translation/test_extG2b_bind.cpp
+++ tests/translation/test_extG2b_bind.cpp
@@ -1,9 +1,14 @@
 #include "runtime.h"
 
-struct Zomg{ char a; int b; };
+struct Zomg
+{
+	char x;
+	char pad_1[3];
+	int y;
+};
 
 Zomg Call_Zomg_ref_Zomg_(Zomg x, void* __context)
 {
-	x.a = x.a == -1 && x.b == -2;
+	x.x = x.x == -1 && x.y == -2;
 	return x;
 }
