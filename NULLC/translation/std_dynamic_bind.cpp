4e13bf881b300fd2c5e09472d043650a04119105
WheretIB <wheretib@gmail.com>
2020-06-15 23:02:15 +0300

    translation to C: updated std.dynamic module bindings

--- NULLC/translation/std_dynamic_bind.cpp
+++ NULLC/translation/std_dynamic_bind.cpp
@@ -1,8 +1,10 @@
 #include "runtime.h"
 
+#undef assert
+
 void  assert(int val, const char* message, void* unused);
 
-void override(NULLCRef a, NULLCRef b, void* unused)
+void override_void_ref_auto_ref_auto_ref_(NULLCRef a, NULLCRef b, void* __context)
 {
 	assert(__nullcGetTypeInfo(a.typeID)->category == NULLC_FUNCTION, "ERROR: first argument to 'override' is not a function", 0);
 	assert(__nullcGetTypeInfo(b.typeID)->category == NULLC_FUNCTION, "ERROR: second argument to 'override' is not a function", 0);
@@ -11,7 +13,7 @@ void override(NULLCRef a, NULLCRef b, void* unused)
 	NULLCFuncPtr<> *bFunc = (NULLCFuncPtr<>*)b.ptr;
 	(*fTable)[aFunc->id] = (*fTable)[bFunc->id];
 }
-void override(NULLCRef function, NULLCArray<char > code, void* unused)
+void override_void_ref_auto_ref_char___(NULLCRef function, NULLCArray< char > code, void* __context)
 {
 	assert(0, "ERROR: override with source code is unsupported in translated code", 0);
 }
